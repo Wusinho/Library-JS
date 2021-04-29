b59bf068dcb11c0bcce338b4f849b6fe680d583f
Carlos-SSH <carlos.anezeh@me.com>
2021-04-29 12:04:58 -0500

    Created prototype and constructor for book.

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+class UI {
+  constructor() {
+      addBooktoList(book){
+        const list = document.getElementById("book-list");
+        const row = document.createElement("tr");
+        row.innerHTML = 
+        `
+          <td> ${book.title} </td>
+          <td> ${book.author} </td>
+          <td> ${book.pages} </td>
+          <td> ${book.read} </td>
+          <td><a href="" class="delete">X</a></td>
+        `;
+      };
+  }
+}
